9b138940d3cc04d4ab2d55308ae2119673b822d1
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-06-30 00:29:18 +0100

    created a basic 4x4 canvas without any numbers or inqualities

--- UI.py
+++ UI.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from calendar import c
+from types import CellType
 from Game import Game
 from Colors import color
 from tkinter import *
@@ -11,6 +13,10 @@ class Ui(ABC):
 
 
 class Gui(Ui):
+    # TODO: create futoshiki grid
+    MARGIN = 50
+    SIDE = 100
+
     def __init__(self):
         self.__game_win = None
         root = Tk()
@@ -30,10 +36,51 @@ class Gui(Ui):
         if self.__game_win:
             return
 
+        WIDTH = HEIGHT = Gui.MARGIN * 2 + Gui.SIDE * 7
         self.__game = Game()
         game_win = Toplevel(self.__root)
         game_win.title("Puzzle")
         game_win.geometry("800x600")
+
+        self.__canvas = Canvas(game_win, width=WIDTH, height=HEIGHT)
+        self.__canvas.pack(fill=BOTH, side=TOP)
+
+        clear_button = Button(game_win, text="Clear answers")
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        self.__draw_grid()
+        self.__draw_puzzle()
+
+        self.__canvas.bind("<Button-1>", self.__cell_clicked)
+        self.__canvas.bind("<Key>", self.__key_pressed)
+
+    def __draw_grid(self):
+        for row in range(self.__game.GRID_SIZE):
+            for l in range(self.__game.GRID_SIZE * 2):
+                # vertical lines
+                x0 = Gui.MARGIN + l * Gui.SIDE
+                y0 = Gui.MARGIN * (row + 1) + Gui.SIDE * row
+                x1 = Gui.MARGIN + l * Gui.SIDE
+                y1 = Gui.MARGIN * (row + 1) + Gui.SIDE * row + Gui.SIDE
+                self.__canvas.create_line(x0, y0, x1, y1)
+
+            for l in range(self.__game.GRID_SIZE):
+                # horizontal lines
+                x0 = Gui.MARGIN + l * 2 * Gui.SIDE
+                y0 = Gui.MARGIN * (row + 1) + Gui.SIDE * row
+                x1 = Gui.MARGIN + l * 2 * Gui.SIDE + Gui.SIDE
+                y1 = Gui.MARGIN * (row + 1) + Gui.SIDE * row
+                self.__canvas.create_line(x0, y0, x1, y1)
+                self.__canvas.create_line(x0, y0 + Gui.SIDE, x1, y1 + Gui.SIDE)
+
+    def __cell_clicked(self):
+        pass
+
+    def __key_pressed(self):
+        pass
+
+    def __draw_puzzle(self):
+        pass
 
     def __quit(self):
         self.__root.quit()
