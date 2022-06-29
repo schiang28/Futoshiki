54b41163bd85e0faee3e06f6220c3e88faf753b1
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-06-29 11:50:53 +0100

    simple gui and home page

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from Game import Game
 from Colors import color
+from tkinter import *
 
 
 class Ui(ABC):
@@ -11,10 +12,34 @@ class Ui(ABC):
 
 class Gui(Ui):
     def __init__(self):
-        pass
+        self.__game_win = None
+        root = Tk()
+        root.title("Futoshiki")
+        root.geometry("500x500")
+        frame = Frame(root)
+        frame.pack()
+
+        Button(frame, text="Play", command=self.__play_game, height=2, width=30).pack(
+            fill=X
+        )
+        Button(frame, text="Quit", command=self.__quit, height=2).pack(fill=X)
+
+        self.__root = root
+
+    def __play_game(self):
+        if self.__game_win:
+            return
+
+        self.__game = Game()
+        game_win = Toplevel(self.__root)
+        game_win.title("Puzzle")
+        game_win.geometry("800x600")
+
+    def __quit(self):
+        self.__root.quit()
 
     def run(self):
-        pass
+        self.__root.mainloop()
 
 
 class Terminal(Ui):
