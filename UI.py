3b1aec9c8c47584010a78717fee56c0110a1f28f
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-07-08 22:55:38 +0100

    able to select grids that are not part of the original filled in puzzle

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from calendar import c
+from turtle import width
 from types import CellType
 from Game import Game
 from Colors import color
@@ -45,10 +46,12 @@ class Gui(Ui):
 
         # geometry has to be a variable for different grid sizes
         game_win.geometry("600x400")
-        WIDTH = HEIGHT = Gui.MARGIN * 2 + Gui.SIDE * (self.__game.GRID_SIZE * 2 - 1)
+        self.__width = self.__height = Gui.MARGIN * 2 + Gui.SIDE * (
+            self.__game.GRID_SIZE * 2 - 1
+        )
 
-        self.__canvas = Canvas(game_win, width=WIDTH, height=HEIGHT)
-        self.__canvas.pack(fill=BOTH, side=LEFT)
+        self.__canvas = Canvas(game_win, width=self.__width, height=self.__height)
+        self.__canvas.pack(side=LEFT)
 
         self.__draw_grid()
         self.__draw_puzzle()
@@ -56,6 +59,7 @@ class Gui(Ui):
         self.__canvas.bind("<Button-1>", self.__cell_clicked)
         self.__canvas.bind("<Key>", self.__key_pressed)
 
+        # stage 2.2 dismiss and check buttons on game window
         self.__game_win = game_win
         dismiss_button = Button(
             game_win, text="Dismiss", command=self.__dismiss_game_win
@@ -94,8 +98,38 @@ class Gui(Ui):
                     tags="numbers",
                 )
 
-    def __cell_clicked(self):
-        pass
+    def __cell_clicked(self, event):
+        if self.__game.check():
+            return
+
+        x, y = event.x, event.y
+        if (
+            Gui.MARGIN < x < self.__width - Gui.MARGIN
+            and Gui.MARGIN < y < self.__height - Gui.MARGIN
+        ):
+            self.__canvas.focus_set()
+
+        self.__row, self.__col = (
+            (y - Gui.MARGIN) // Gui.SIDE,
+            (x - Gui.MARGIN) // Gui.SIDE,
+        )
+        self.__draw_cursor()
+
+    def __draw_cursor(self):
+        self.__canvas.delete("cursor")  # clears previous cursors
+        # checks row and col in canvas and is a grid rather than in between grid, and if it is empty
+        if (
+            self.__row >= 0
+            and self.__col >= 0
+            and self.__row % 2 == 0
+            and self.__col % 2 == 0
+            and self.__game.file[self.__row][self.__col] == self.__game.EMPTY
+        ):
+            x0 = Gui.MARGIN + self.__col * Gui.SIDE + 1
+            y0 = Gui.MARGIN + self.__row * Gui.SIDE + 1
+            x1 = Gui.MARGIN + (self.__col + 1) * Gui.SIDE - 1
+            y1 = Gui.MARGIN + (self.__row + 1) * Gui.SIDE - 1
+            self.__canvas.create_rectangle(x0, y0, x1, y1, outline="red", tags="cursor")
 
     def __key_pressed(self):
         pass
